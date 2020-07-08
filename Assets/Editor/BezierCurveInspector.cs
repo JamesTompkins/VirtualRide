d43e5748a13f19ab128359a6de9fed0aa30b4051
James Tompkins <jamestompkins@hotmail.co.uk>
2020-07-08 14:43:50 -0500

    added bezier spline

--- Assets/Editor/BezierCurveInspector.cs
+++ Assets/Editor/BezierCurveInspector.cs
@@ -8,6 +8,7 @@ using System;
 public class BezierCurveInspector : Editor
 {
     private const int lineSteps = 10;
+    private const float directionScale = 0.5f;
 
 
     private BezierCurve curve;
@@ -23,13 +24,17 @@ public class BezierCurveInspector : Editor
         Vector3 p0 = ShowPoint(0);
         Vector3 p1 = ShowPoint(1);
         Vector3 p2 = ShowPoint(2);
+        Vector3 p3 = ShowPoint(3);
 
         Handles.color = Color.gray;
         Handles.DrawLine(p0, p1);
-        Handles.DrawLine(p1, p2);
+        Handles.DrawLine(p2, p3);
+
+        ShowDirections();
+        Handles.DrawBezier(p0, p3, p1, p2, Color.white, null, 2f);
 
 
-        Vector3 lineStart = curve.GetPoint(0f);
+        /*Vector3 lineStart = curve.GetPoint(0f);
         Handles.color = Color.green;
         Handles.DrawLine(lineStart, lineStart + curve.GetDirection(0f));
         for (int i=1; i<=lineSteps; i++) {
@@ -45,6 +50,17 @@ public class BezierCurveInspector : Editor
             Handles.DrawLine(lineStart, lineStart + curve.GetDirection(iNormal));
 
             lineStart = lineEnd;
+        }*/
+    }
+
+    private void ShowDirections() {
+        Handles.color = Color.green;
+        Vector3 point = curve.GetPoint(0f);
+        Handles.DrawLine(point, point + curve.GetDirection(0f) * directionScale);
+        for (int i=1; i <= lineSteps; i++) {
+            float iNormal = i / (float)lineSteps;
+            point = curve.GetPoint(iNormal);
+            Handles.DrawLine(point, point + curve.GetDirection(iNormal) * directionScale);
         }
     }
 
--- Assets/Scripts/Bezier.cs
+++ Assets/Scripts/Bezier.cs
@@ -16,13 +16,16 @@ public static class Bezier {
         //return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1,p2,t), t);
     }
 
-    public static Vector3 getFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t) {
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, float t) {
         return  2f * (1f - t) * (p1 - p0) +
                 2f * t * (p2 - p1);
     }
 
 
 
+    //================================//
+    //==========cubic curves==========//
+    //================================//
 
     public static Vector3 GetPoint(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
         t = Mathf.Clamp01(t);
@@ -34,7 +37,7 @@ public static class Bezier {
                 t * t * t * p3;
     }
 
-    public static Vector3 GetFirstDerivative (Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
+    public static Vector3 GetFirstDerivative(Vector3 p0, Vector3 p1, Vector3 p2, Vector3 p3, float t) {
         t = Mathf.Clamp01(t);
         float oneMinusT = 1f - t;
         return
